0d34f9eba90518ae686ccd6175f6f3f99a07a90f
luke-sage-kim <118151831+luke-sage-kim@users.noreply.github.com>
2023-04-20 09:53:19 +0900

    test2

--- s20230404/src/main/java/com/travelAlone/s20230404/service/mh/mhServiceImpl.java
+++ s20230404/src/main/java/com/travelAlone/s20230404/service/mh/mhServiceImpl.java
@@ -41,6 +41,7 @@ public class mhServiceImpl implements mhService {
 		System.out.println("mhServiceImpl detail ...");
 		Notice notice = null;
 		notice = mh.detailNotice(g_notice_id);
+		//test
 		return notice;						
 	}
 
