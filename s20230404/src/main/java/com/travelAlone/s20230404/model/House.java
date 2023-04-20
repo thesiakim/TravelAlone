c24a0fd8a64edcee207ab29ace5ff93bbc0d1068
thesiakim <122683107+thesiakim@users.noreply.github.com>
2023-04-20 18:04:09 +0900

    "수정본 "

--- s20230404/src/main/java/com/travelAlone/s20230404/model/House.java
+++ s20230404/src/main/java/com/travelAlone/s20230404/model/House.java
@@ -31,29 +31,6 @@ public class House {
 	private String pageNum;  
 	//            1                           10
 	private int start; 		 	private int end;
-	
-	private long house_id;
-	private String h_name;
-	private String h_content;
-	private String h_address;
-	private String h_checkinout;
-	private String h_call;
-	private String h_room;
-	private String h_eat;
-	private String h_parking;
-	private String h_common_house;
-	private String h_common_loc;
-    private LocalDateTime  create_date;
-    private LocalDateTime  modified_date;
-    private int h_count; 
-    
-    // 페이지조회용
-    private String search;      private String keyword;
-    private String pageNum;  
-    //            1                          10
-    private int start;           private int end;
-
-
 
 }
 
